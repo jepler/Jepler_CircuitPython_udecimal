295c5792654c40bfa208d752d89bf4f4703f2810
Jeff Epler <jepler@gmail.com>
2020-09-06 16:30:47 -0500

    correct package metadata

--- setup.py
+++ setup.py
@@ -23,18 +23,18 @@ with open(path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name="adafruit-circuitpython-udecimal",
+    name="jepler-circuitpython-udecimal",
     use_scm_version=True,
     setup_requires=["setuptools_scm"],
     description="Reduced version of the decimal library for CircuitPython",
     long_description=long_description,
     long_description_content_type="text/x-rst",
     # The project's main homepage.
-    url="https://github.com/adafruit/Adafruit_CircuitPython_udecimal",
+    url="https://github.com/jepler/Jepler_CircuitPython_udecimal",
     # Author details
-    author="Adafruit Industries",
-    author_email="circuitpython@adafruit.com",
-    install_requires=["Adafruit-Blinka",],
+    author="Jeff Epler",
+    author_email="jepler@gmail.com",
+    install_requires=[],
     # Choose your license
     license="MIT",
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
@@ -49,8 +49,8 @@ setup(
         "Programming Language :: Python :: 3.5",
     ],
     # What does your project relate to?
-    keywords="adafruit blinka circuitpython micropython udecimal numeric helper arbitrary- "
-    "precision math",
+    keywords="adafruit blinka circuitpython micropython udecimal numeric helper "
+    "arbitraryprecision math",
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=["jepler_udecimal"],
