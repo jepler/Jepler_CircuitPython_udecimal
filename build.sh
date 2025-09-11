1d08c653f692d11a9a35e29cfcd06b43db5d5150
Jeff Epler <jepler@unpythonic.net>
2025-09-11 10:50:11 -0500

    build: Revamp
    it's been broken since the switch to pyproject.

--- build.sh
+++ build.sh
@@ -1,54 +1,80 @@
 #!/bin/bash
 set -eo pipefail
 
-repo_name=`git config remote.origin.url | sed -e 's,^.*/,,;s/\.git$//' | tr A-Z_ a-z-`
-echo "=== Building package: $repo_name"
+in_section=false
+if [ "${CI}" -eq "true" ]; then
+    start_section () {
+        if $in_section; then end_section; fi
+        echo "::group::$*"
+        export in_section=true
+    }
+    end_section () {
+        echo "::endgroup::$*"
+        in_section=false
+    }
+else
+    start_section () {
+        if $in_section; then end_section; fi
+        echo "=== $*"
+        export in_section=true
+    }
+    end_section () {
+        echo
+        in_section=false
+    }
+fi
 
-echo "=== Create and set up virtual environment"
+repo_name=`git config remote.origin.url | sed -e 's,^.*/,,;s/\.git$//' | tr A-Z_ a-z-`
+echo "*** Building package: $repo_name"
+
+start_section "Create and set up virtual environment"
 [ -d .venv ] || python3 -m venv .env
 . .env/bin/activate
-echo "=== Install requirements"
+start_section "Install requirements"
 pip3 install wheel
-pip3 install -r requirements_dev.txt
-echo "=== Run pre-commit"
+pip3 install -r optional_requirements.txt
+start_section "Run pre-commit"
 pre-commit run --all-files
 
-echo "=== Clone and build circuitpython unix port"
+start_section "Clone and build circuitpython unix port"
 [ -e circuitpython/py/py.mk ] || git clone --shallow-since=2021-07-01 https://github.com/adafruit/circuitpython
 [ -e circuitpython/lib/libffi/autogen.sh ] || (cd circuitpython && git submodule update --init lib/libffi lib/axtls lib/berkeley-db-1.xx tools/huffman lib/uzlib extmod/ulab)
 [ -x circuitpython/ports/unix/micropython ] || (
 make -C circuitpython/mpy-cross -j$(nproc)
-make -C circuitpython/ports/unix -j$(nproc) deplibs
+make -C circuitpython/ports/unix -j$(nproc) deplibs submodules
 make -C circuitpython/ports/unix -j$(nproc) DEBUG=1 STRIP=:
 )
 
-echo "=== Run tests"
+start_section "Run tests"
 python -m jepler_udecimal.test
 
-run-tests () {
-    env MICROPYPATH=. PYTHONPATH=. MICROPY_MICROPYTHON=circuitpython/ports/unix/micropython circuitpython/tests/run-tests.py "$@"
+run_tests () {
+    env MICROPYPATH="`readlink -f .`" PYTHONPATH="`readlink -f .`" MICROPY_MICROPYTHON=circuitpython/ports/unix/build-standard/micropython circuitpython/tests/run-tests.py --keep-path "$@"
 }
 
-run-tests --clean-failures
-if ! run-tests -d examples; then
-    run-tests --print-failures
+run_tests --clean-failures
+if ! run_tests -d examples; then
+    run_tests --print-failures
     exit 1
 fi
 
 PYTHONPATH=. python examples/test_udecimal.py > test_udecimal.exp
-echo "=== Build CircuitPython bundle"
+start_section "Build CircuitPython bundle"
 circuitpython-build-bundles --package_folder_prefix jepler --filename_prefix $repo_name --library_location .
 
-echo "=== Build docs"
+start_section "Build docs"
 rm -rf docs/_build
 (cd docs && sphinx-build -E -W -b html . _build/html)
 
-echo "=== Build pypi files"
-python setup.py sdist
-python setup.py bdist_wheel --universal
+start_section "Build pypi files"
+python -m build
 
-echo "=== Check pypi files"
+start_section "Check pypi files"
 twine check dist/*
+end_section
+
+echo "=== Success"
+
 # SPDX-FileCopyrightText: Copyright (c) 2020 jepler for Unpythonic Networks
 #
 # SPDX-License-Identifier: MIT
